6d95c9368210da069994d35e7e08c3d56c977d8a
HY <e0726431@u.nus.edu>
2023-07-08 05:52:59 +0800

    add methodology

--- pages/1_📊_Sights.py
+++ pages/1_📊_Sights.py
@@ -24,7 +24,86 @@ st.markdown(
 """
 )
 st.plotly_chart(overview_plot, theme=None)
+with st.expander("Methodology"):
+    st.markdown(
+        """
+        1. Each image is loaded using an image dataloader and custom dataset class for memory-efficient loading, with the help of PyTorch.
+            - Each image is processed and resized to size 3 x 224 x 224.
+            - Greyscale Images are converted to RGB format. 
+        <details><summary>Show Code</summary>
 
+        ```python
+        from torch.utils.data import Dataset
+        class CustomImageFolder(Dataset):
+            TF = transforms.Compose(
+                [transforms.Resize(
+                    (224, 224), antialias=True), transforms.ToTensor()]
+            )
+            def __init__(self, img_dir, csv_dir, transform=TF):
+                # ...
+                self.transform = transform
+
+            def __getitem__(self, idx):
+                try:
+                    img_link = self.data[idx]
+                    img_id = int(img_link.split(".")[0]) # Gets image ID, for example 100346
+                    img = (read_image(self.img_dir + "/" + img_link, ImageReadMode.RGB))
+                # ...
+        ```
+        </details>
+
+        2. ResNet152, a deep neural network used in image recognition is loaded, and its final fully-connected layer is removed.
+        <details><summary>Show Code</summary>
+
+        ```python
+        import pytorch
+        from torchvision import models
+        model = models.resnet152(weights=models.ResNet152_Weights.DEFAULT)
+        # Remove final layer
+        newmodel = torch.nn.Sequential(
+            OrderedDict([*(list(model.named_children())[:-1])])
+        )
+        ```
+        </details>
+
+        3. Images are loaded as a tensor into ResNet152 in batches of 64 and fed through the model. The output of each image is a vector of length 2048.
+        <details><summary>Show Code</summary>
+
+        ```python
+        datapoints = []
+        list_labels = []
+        with torch.no_grad():
+        # ...
+        newmodel.to("cuda") # Using GPU
+        for i, (features, labels) in enumerate(dataloader): 
+            # dataloader is a pytorch dataloader object.
+            features = features.to("cuda") 
+            # features is a batch of 64 images as (3 x 224 x 224) tensors
+            outputs = np.squeeze(
+                newmodel(features).to("cpu").numpy())
+                .tolist() # A vector
+            datapoints.extend(outputs)
+            list_labels.extend(labels)
+        ```
+        </details>
+
+        4. The resulting vectors are condensed into 3 dimensions using sklearn's t-SNE and plotted using Plotly's 3D scatterplot.
+            - This plot is saved as a json file.
+
+        <details><summary>Show Code</summary>
+
+        ```python
+        from sklearn.manifold import TSNE
+        tsne = TSNE(n_components=3, verbose=1, perplexity=40, n_iter=400)
+        X = tsne.fit_transform(X_prior)
+        fig = px.scatter_3d(...)
+        fig.write_json(...)
+        ```
+        </details>
+
+        > More details can be found on Github.
+    """,
+    unsafe_allow_html=True)
 st.subheader("Colours")
 st.divider()
 st.write("Let's explore the colours of the artworks in MoMa's art collection.")
@@ -105,6 +184,5 @@ st.write(
 )
 
 st.divider()
-####################################################################################################################################
 
 
