75969295ff56686e9e893de739e6674f9b2de452
HY <e0726431@u.nus.edu>
2023-07-08 06:22:03 +0800

    resize and update scatterplot

--- overview/overview_preprocessing.py
+++ overview/overview_preprocessing.py
@@ -79,7 +79,7 @@ def run():
         z="x3", 
         color='Classification', 
         custom_data=['id'], 
-        width=800, 
+        width=1000, 
         height=800,
         text=dataframe['Classification']
     )
