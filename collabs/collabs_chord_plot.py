8c6617a7042c548091621cbf3e02c696d523bdb8
Valerie Tan <valerietanhx@gmail.com>
2023-07-17 22:57:23 +0800

    Fix typo in chord diagram

--- collabs/collabs_chord_plot.py
+++ collabs/collabs_chord_plot.py
@@ -4,15 +4,15 @@ import holoviews as hv
 import numpy as np
 import pandas as pd
 from bokeh.plotting import show
-from holoviews import dim
 
 hv.extension("bokeh")
 hv.output(size=200)
 
-edges = pd.read_csv("collabs/CollabsGraphEdgesV2.csv")
+edges = pd.read_csv("collabs/CollabsGraphEdges.csv")
 edges = edges.query("`value` >= 20")  # avoid plot looking like a yarn ball
 
 chord = hv.Chord((edges))
+
 
 # https://stackoverflow.com/questions/65561927/inverted-label-text-half-turn-for-chord-diagram-on-holoviews-with-bokeh/65610161#65610161
 def rotate_label(plot, element):
@@ -38,14 +38,14 @@ def rotate_label(plot, element):
     angles[np.where((angles < -math.pi / 2) | (angles > math.pi / 2))] += math.pi
     plot.handles["text_1_glyph"].text_align = "center"
 
+
 chord.opts(
     cmap="Category20",
     edge_cmap="Category20",
-    edge_color=dim("source").str(),
+    edge_color="source",
     labels="index",
-    node_color=dim("index").str(),
+    node_color="index",
     hooks=[rotate_label],
 )
-
 
 show(hv.render(chord))
